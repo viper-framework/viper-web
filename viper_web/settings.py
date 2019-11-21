b6c5839ed542dc07394f59512320bc80394d6ce2
Joel Snape <jsnape@nettitude.com>
2019-11-21 16:44:31 +0000

    Fix for API documentation https://www.django-rest-framework.org/community/3.10-announcement/#continuing-to-use-coreapi

--- viper_web/settings.py
+++ viper_web/settings.py
@@ -98,6 +98,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 8,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 SWAGGER_SETTINGS = {
